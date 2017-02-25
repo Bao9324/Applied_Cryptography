cdc74884f9ad0ce6c18d3ffa506110e5acf98dd5
andy <at2706@nyu.edu>
2017-02-25 13:57:52 -0500

    Letter Frequencies

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,4 +1,38 @@
 #include "main.h"
 int main(int argc, char const *argv[]) {
+	string cipher;
+	cout << "Enter the ciphertext: ";
+	cin >> cipher;
+
+	vector<int> result = split_cipher(cipher);
+	map<int, int> freq = map_frequency(result);
+	for (pair<int, int> i : freq)
+		cout << i.first << ',' << i.second << endl;
+
+	cin >> cipher;
 	return 0;
+}
+
+vector<int> split_cipher(string cipher) {
+	vector<int> v;
+
+	int pos = 0;
+	int next_pos = 0;
+	do {
+		next_pos = cipher.find(',', pos);
+		int num = stoi(cipher.substr(pos, next_pos));
+		v.push_back(num);
+		pos = next_pos + 1;
+	} while (pos != 0);
+
+	return v;
+}
+
+map<int, int> map_frequency(vector<int> cipher) {
+	map<int, int> m;
+
+	for (int i : cipher)
+		m[i]++;
+
+	return m;
 }
--- Project1/Project1/main.h
+++ Project1/Project1/main.h
@@ -1,4 +1,41 @@
 #pragma once
-#include <iostream>
 #include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
 using namespace std;
+
+map<char, int> avg_freqencies = {
+	pair<char, int>(' ', 19), 
+	pair<char, int>('a', 7), 
+	pair<char, int>('b', 1),
+	pair<char, int>('c', 2),
+	pair<char, int>('d', 4),
+	pair<char, int>('e', 10),
+	pair<char, int>('f', 2),
+	pair<char, int>('g', 2),
+	pair<char, int>('h', 5),
+	pair<char, int>('i', 6),
+	pair<char, int>('j', 1),
+	pair<char, int>('k', 1),
+	pair<char, int>('l', 3),
+	pair<char, int>('m', 2),
+	pair<char, int>('n', 6),
+	pair<char, int>('o', 6),
+	pair<char, int>('p', 2),
+	pair<char, int>('q', 1),
+	pair<char, int>('r', 5),
+	pair<char, int>('s', 5),
+	pair<char, int>('t', 7),
+	pair<char, int>('u', 2),
+	pair<char, int>('v', 1),
+	pair<char, int>('w', 2),
+	pair<char, int>('x', 1),
+	pair<char, int>('y', 2),
+	pair<char, int>('z', 1)};
+map<char, int[]> key;
+
+vector<int> split_cipher(string cipher);
+map<int, int> map_frequency(vector<int> cipher);
